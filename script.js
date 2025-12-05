6bff9ab0f264925632e17314dde643b815a45ce5
madbark67 <239074869+madbark67@users.noreply.github.com>
2025-12-04 20:59:46 -0500

    Implement rainbow coloring toggle and error validations
    Created a randomized RGB generator with Math.random()
    and conditional IF to determine whether the effect
    is toggled on or off. Added ability to generate
    new 16x16 grid if prompt is submitted as blank.

--- script.js
+++ script.js
@@ -2,13 +2,30 @@ const container = document.querySelector(".container");
 const header = document.querySelector(".header");
 
 const input = document.createElement("button");
-input.textContent = "New";
+input.textContent = "New Grid";
 header.appendChild(input);
+
+const checkbox = document.createElement("input");
+checkbox.type = "checkbox";
+header.appendChild(checkbox);
+
+const rgbLabel = document.createElement("label");
+rgbLabel.textContent = "Rainbow Mode";
+header.appendChild(rgbLabel);
+let rgbCheck = false;
+
+checkbox.addEventListener("change", (e) => {
+    rgbCheck = e.target.checked ? true : false;
+});
 
 generateSquareGrid(16);
 
 container.addEventListener("mouseover", (e) => {
-    e.target.classList.add('color');
+    if (rgbCheck) {
+        e.target.style.backgroundColor = generateRandomRGB();
+    } else if (!rgbCheck) {
+        e.target.style.backgroundColor = "black";
+    }
 });
 
 input.addEventListener("click", () => {
@@ -16,15 +33,25 @@ input.addEventListener("click", () => {
     while (width > 100) {
         width = prompt("Value must be below 100!");
     }
+    if (!width) {
+        width = 16;
+    }
     generateSquareGrid(width);
 });
 
 function generateSquareGrid(width) {
     container.replaceChildren();
-    for (let i = 0; i < width**2; i++) {
+    for (let i = 0; i < width ** 2; i++) {
         let square = document.createElement("div");
         square.className = "square";
         square.style.flexBasis = `calc(100%/${width})`;
         container.appendChild(square);
     }
+}
+
+function generateRandomRGB() {
+    let red = Math.floor(Math.random() * 256);
+    let green = Math.floor(Math.random() * 256);
+    let blue = Math.floor(Math.random() * 256);
+    return `rgb(${red}, ${green}, ${blue})`;
 }
